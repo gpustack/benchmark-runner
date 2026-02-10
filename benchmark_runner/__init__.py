0b055ae39cbff25befaef23c6625d6d09805d657
michelia <michelia@seal.io>
2026-02-10 17:39:00 +0800

    chore: rename output_summary_json->output_dual_json

--- benchmark_runner/__init__.py
+++ benchmark_runner/__init__.py
@@ -2,4 +2,4 @@ __version__ = '0.0.0'
 __git_commit__ = 'HEAD'
 
 from . import custom_response_handler  # noqa: F401 # Register custom response handler
-from . import output_summary_json  # noqa: F401 # Register output format
+from . import output_dual_json  # noqa: F401 # Register output format
